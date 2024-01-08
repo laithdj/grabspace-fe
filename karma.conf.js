5a5d07fadd21051f2b7d870e8d5f64071759abd4
Rahul Pandya <pnd.rahul@gmail.com>
2024-01-08 17:49:53 +0530

    initial commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/realestate'),
+      dir: require('path').join(__dirname, './coverage/grabspace'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'realestate'`, () => {
+  it(`should have as title 'grabspace'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('realestate');
+    expect(app.title).toEqual('grabspace');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('realestate app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('grabspace app is running!');
   });
 });
